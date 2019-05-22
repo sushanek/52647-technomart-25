fcdc93efe56a9a03249fab6a455b4bd0c28d7c41
Lev <47941375+sushanek@users.noreply.github.com>
2019-05-22 13:04:27 +0300

    LiveView

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let gulp = require("gulp");
+let server = require("browser-sync").create();
+
+
+gulp.task("server", function () {
+  server.init({
+    server: "/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("*.html").on("change", server.reload);
+});
