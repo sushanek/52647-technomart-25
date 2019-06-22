715f8b16636a88b7b02ac3192d522507ba0fa4f2
Lev <47941375+sushanek@users.noreply.github.com>
2019-06-22 19:33:00 +0300

    Доделка слайдер

--- js/script.js
+++ js/script.js
@@ -10,18 +10,55 @@ var btnModalMap = document.querySelector(".btn-modal-map");
 var modalMap = document.querySelector(".modal-map");
 var btnMapClose = modalMap.querySelector(".btn-close");
 
-var btnSlider = document.querySelector(".slider-left");
-var vistaSlider = document.querySelector(".vista-slider");
-/* var btnToBasket = document.querySelectorAll(".to-basket");
-var modalCart = document.querySelector(".modal-basket");
- */
+var btnDelivery = document.querySelector(".btn-delivery");
+var btnGuarantee = document.querySelector(".btn-guarantee");
+var btnCredit = document.querySelector(".btn-credit");
+var serviceDelivery = document.querySelector(".service-delivery");
+var serviceGuarantee = document.querySelector(".service-guarantee");
+var serviceCredit = document.querySelector(".service-credit");
+
+btnDelivery.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  btnDelivery.classList.add("service-item__current");
+  btnGuarantee.classList.remove("service-item__current");
+  btnCredit.classList.remove("service-item__current");
+  serviceDelivery.classList.remove("visually-hidden");
+  serviceGuarantee.classList.add("visually-hidden");
+  serviceCredit.classList.add("visually-hidden");
+});
+
+btnGuarantee.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  btnDelivery.classList.remove("service-item__current");
+  btnGuarantee.classList.add("service-item__current");
+  btnCredit.classList.remove("service-item__current");
+  serviceDelivery.classList.add("visually-hidden");
+  serviceGuarantee.classList.remove("visually-hidden");
+  serviceCredit.classList.add("visually-hidden");
+});
+
+btnCredit.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  btnDelivery.classList.remove("service-item__current");
+  btnGuarantee.classList.remove("service-item__current");
+  btnCredit.classList.add("service-item__current");
+  serviceDelivery.classList.add("visually-hidden");
+  serviceGuarantee.classList.add("visually-hidden");
+  serviceCredit.classList.remove("visually-hidden");
+});
+
+
+/* var btnSlider = document.querySelector(".promo-slider-nav");
+var vistaSlider1 = document.querySelector(".vista-slider-1");
+var vistaSlider1 = document.querySelector(".vista-slider-2");
+
 
 btnSlider.addEventListener("click", function (evt) {
   evt.preventDefault();
-  vistaSlider.classList.toggle("vista-slider-1");
-  vistaSlider.classList.toggle("vista-slider-2");
+  vistaSlider1.classList.toggle("modal-show");
+  vistaSlider2.classList.toggle("modal-show");
 });
-
+ */
 
 //Окно обратной связи
 btnModalLetter.addEventListener("click", function (evt) {
